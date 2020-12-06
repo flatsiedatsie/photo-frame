b885ca6bd2492680be1364f0f0e03bab58366ae1
flatsiedatsie <unfold@gmail.com>
2020-12-06 23:28:53 +0100

    Add files via upload

--- package.sh
+++ package.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
 # Setup environment for building inside Dockerized toolchain
 [ $(id -u) = 0 ] && umask 0
 
 # Clean up from previous releases
-rm -rf *.tgz ._* *.pyc *.sha256sum package SHA256SUMS lib
+rm -rf *.tgz *.sha256sum package SHA256SUMS lib
 
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
@@ -14,37 +15,33 @@ else
   TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
 fi
 
-rm -rf SHA256SUMS package
-rm -rf ._*
-rm -rf .tgz
-rm -rf *.pyc
 
-# for privacy, remove my own photos?
-rm -rf photos
-mkdir -p photos
 
-mkdir -p lib package
-mkdir -p package/photos
+# Prep new package
+mkdir lib package
+
 # Pull down Python dependencies
 pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
-cp *.py manifest.json LICENSE README.md package/
-cp -r pkg lib css images js views package/
+# Put package together
+cp -r lib pkg LICENSE manifest.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
+find package -type d -empty -delete
+rm -rf package/pkg/pycache
 
+# Generate checksums
 echo "generating checksums"
 cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
 
-echo "creating archive"
-
 # Make the tarball
-sha256sum "photo-frame-${version}.tgz"	echo "creating archive"
-TARFILE="photo-frame-${version}.tgz"
+echo "creating archive"
+TARFILE="mysensors-adapter-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
 
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat ${TARFILE}.sha256sum
-
+#sha256sum ${TARFILE}
+#rm -rf SHA256SUMS package
